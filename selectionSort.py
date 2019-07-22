7cd0c2905359ceffe92a9a6d590c4e19ebecaa8f
divyaswormakai <makaidivya@gmail.com>
2019-07-22 08:34:03 +0545

    dfs and bfs

--- selectionSort.py
+++ selectionSort.py
@@ -7,21 +7,9 @@ def selectionSort(array: list) -> list:
 		array[i], array[minIndex] = array[minIndex], array[i]
 
 
-def recursiveSelectionSort(array: list) -> list:
-	if len(array) > 1:
-		minIndex = 0
-		for index in range(1, len(array)):
-			if array[index] < array[minIndex]:
-				minIndex = index
-		array[0], array[minIndex] = array[minIndex], array[0]
-		array = array[:1] + recursiveSelectionSort(array[1:])
-	return array
-
-
 if __name__ == '__main__':
 	# array = random.sample(range(1, 1000), 500)
 	array = [5, 3, 2, 10 , 1 , 9]
 	selectionSort(array)
 	print(array)
-	print(recursiveSelectionSort(array))
 	
